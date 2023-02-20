765cd669dcddf2a183e7115e081dd79bab339057
4kin-home <4kin.nikita@gmail.com>
2023-02-21 02:39:40 +0300

    на работу видео 7 время 02-22

--- src/main/java/guru/springfamework/api/v1/mapper/CategoryMapper.java
+++ src/main/java/guru/springfamework/api/v1/mapper/CategoryMapper.java
@@ -2,13 +2,14 @@ package guru.springfamework.api.v1.mapper;
 
 import guru.springfamework.api.v1.model.CategoryDTO;
 import guru.springfamework.domain.Category;
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 /**
  * Created by jt on 9/25/17.
  */
-@Mapper
+@Mapper(injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface CategoryMapper {
 
     CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
